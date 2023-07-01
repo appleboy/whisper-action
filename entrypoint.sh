4e10a258aee3ec5daec44672bf6752a31cff8e06
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-01 18:19:02 +0800

    chore: improve performance and testing across platforms
    - Check if model files exist before downloading them in `entrypoint.sh`
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,15 @@ model=${INPUT_MODEL:-"small"}
 [ -d models ] || mkdir -p models
 
 case "$model" in
-  small) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin \
+  small) [ -f "models/ggml-small.bin" ] || curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin \
       --output models/ggml-small.bin
       export INPUT_MODEL=models/ggml-small.bin
     ;;
-  medium) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin \
+  medium) [ -f "models/ggml-medium.bin" ] || curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin \
       --output models/ggml-medium.bin
       export INPUT_MODEL=models/ggml-medium.bin
     ;;
-  large) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large.bin \
+  large) [ -f "models/ggml-large.bin" ] || curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large.bin \
       --output models/ggml-large.bin
       export INPUT_MODEL=models/ggml-large.bin
     ;;
