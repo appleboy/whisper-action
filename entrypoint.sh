ebc5f9121143d8d85d823c88dd3652443272a50a
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-01 17:54:15 +0800

    ci: refactor model handling and GitHub workflow
    - Remove model download step from `youtube.yml` workflow
    - Add `models` to `.gitignore`
    - Implement model selection and download in `entrypoint.sh`
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,23 @@
 
 set -eu
 
+model=${INPUT_MODEL:-"small"}
+
+[ -d models ] || mkdir -p models
+
+case "$model" in
+  small) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin \
+      --output models/ggml-small.bin
+      export INPUT_MODEL=models/ggml-small.bin
+    ;;
+  medium) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin \
+      --output models/ggml-medium.bin
+      export INPUT_MODEL=models/ggml-medium.bin
+    ;;
+  large) curl -LJ https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large.bin \
+      --output models/ggml-large.bin
+      export INPUT_MODEL=models/ggml-large.bin
+    ;;
+esac
+
 sh -c "/bin/go-whisper $*"
